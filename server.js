a0163d47d0b984c01bde18323bf3f9bc95449826
Jake Ascher <ascherj@gmail.com>
2019-02-25 16:52:55 -0500

    Delete submodules, move proxy files, and update index.html and server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 8081;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
